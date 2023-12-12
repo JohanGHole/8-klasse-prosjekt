dae6ee536eef45728d29ab29a79c4d5756de3ee3
Johan Hole <36231049+JohanGHole@users.noreply.github.com>
2023-12-12 11:11:32 +0100

    karaktervelger

--- main.ts
+++ main.ts
@@ -1,40 +1,37 @@
 namespace SpriteKind {
     export const SelectableCharacter = SpriteKind.create()
 }
+let showCharacterScreen = false
+let and: Sprite = null
+let textSprite: TextSprite = null
+let ourCharacters: Sprite[] = []
+let current_character_index = 0
 function show_character_picker () {
     scene.centerCameraAt(80, 60)
     showCharacterScreen = true
-    potet = sprites.create(assets.image`Potet`, SpriteKind.SelectableCharacter)
-    potet.setPosition(32, 67)
+    and = sprites.create(img`
+        . . . . . . . . . . b 5 b . . . 
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . . . . b c . . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . . . . b b 5 d 1 f 5 5 d f . . 
+        . . . . b 5 5 1 f f 5 d 4 c . . 
+        . . . . b 5 5 d f b d d 4 4 . . 
+        b d d d b b d 5 5 5 4 4 4 4 4 b 
+        b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+        b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+        c d d c d 5 5 b 5 5 5 5 5 5 b . 
+        c b d d c c b 5 5 5 5 5 5 5 b . 
+        . c d d d d d d 5 5 5 5 5 d b . 
+        . . c b d d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
+        `, SpriteKind.SelectableCharacter)
+    and.z = 101
+    and.setPosition(32, 67)
+    textSprite = textsprite.create("Velg din karakter:")
+    textSprite.z = 101
+    and.setPosition(80, 29)
+    ourCharacters = [and]
+    current_character_index = 1
 }
-let potet: Sprite = null
-let showCharacterScreen = false
-scene.setBackgroundColor(15)
-game.splash("Welcome")
-show_character_picker()
-forever(function () {
-    let mySprite: Sprite = null
-    animation.runImageAnimation(
-    mySprite,
-    [img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `],
-    500,
-    false
-    )
-})
