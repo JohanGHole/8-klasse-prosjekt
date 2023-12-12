1101dd52755471e76464c7867517d15df1e1befb
Johan Hole <36231049+JohanGHole@users.noreply.github.com>
2023-12-12 09:55:47 +0100

    Start animasjon

--- main.ts
+++ main.ts
@@ -1,2 +1,27 @@
 music.play(music.createSong(hex`00500004080200`), music.PlaybackMode.UntilDone)
 let mySprite = sprites.create(assets.image`Smil`, SpriteKind.Player)
+forever(function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `],
+    500,
+    false
+    )
+})
