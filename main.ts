96882ca33858674704d39bf56f3e55a6c2f35924
Johan Hole <36231049+JohanGHole@users.noreply.github.com>
2024-01-09 10:50:23 +0100

    oppdatert meny

--- main.ts
+++ main.ts
@@ -238,3 +238,4 @@ scene.setBackgroundImage(img`
     ................................................................................................................................................................
     `)
 show_character_picker()
+music.play(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
