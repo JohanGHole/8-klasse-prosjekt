5e89c049296a64ce71c4f8dbce751e0efba234d7
Johan Hole <36231049+JohanGHole@users.noreply.github.com>
2024-02-08 15:04:48 +0100

    fix: working state with character placeholders

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -1,5 +1,35 @@
 // Auto-generated code. Do not edit.
 namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`0800080000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000000000202020202020202`, img`
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.builtin.forestTiles0,sprites.castle.tilePath5], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myTile":
+            case "tile1":return tile1;
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
 
 }
 // Auto-generated code. Do not edit.
