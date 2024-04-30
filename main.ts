bf65c5bc8a1e6d8b9814722ae5ffe6ada49d9d97
Johan Hole <36231049+JohanGHole@users.noreply.github.com>
2024-04-30 11:24:01 +0200

    Update pxt.json, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,7 @@ namespace SpriteKind {
     export const SelectableCharacter = SpriteKind.create()
     export const Background = SpriteKind.create()
 }
-
-controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
-    
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (showCharacterScreen) {
         thePlayer = sprites.readDataSprite(currently_selected_character, "player")
         thePlayer.setFlag(SpriteFlag.Invisible, false)
@@ -21,140 +19,111 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
         }
         controller.moveSprite(thePlayer, 100, 0)
         showCharacterScreen = false
-        music.play(music.createSong(assets.song`
-                Backgrounds music
-            `), music.PlaybackMode.UntilDone)
+        music.play(music.createSong(assets.song`Backgrounds music`), music.PlaybackMode.UntilDone)
     } else {
         thePlayer.vy = -100
     }
-    
 })
-controller.left.onEvent(ControllerButtonEvent.Pressed, function on_left_pressed() {
-    
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     if (showCharacterScreen) {
         current_character_index = (current_character_index + (ourCharacters.length - 1)) % ourCharacters.length
     }
-    
 })
-function show_character_picker() {
-    
+function show_character_picker () {
     scene.centerCameraAt(80, 60)
     for (let value4 of sprites.allOfKind(SpriteKind.Player)) {
         value4.ay = gravity
         value4.setFlag(SpriteFlag.Invisible, true)
     }
     showCharacterScreen = true
-    karakter1 = sprites.create(assets.image`
-            karakter1
-        `, SpriteKind.SelectableCharacter)
+    karakter1 = sprites.create(assets.image`karakter1`, SpriteKind.SelectableCharacter)
     karakter1.z = 101
     karakter1.setPosition(32, 67)
     sprites.setDataSprite(karakter1, "player", karakter1_liten)
     textSprite = textsprite.create("Velg din karakter:", 9, 10)
     textSprite.z = 101
     textSprite.setPosition(80, 29)
-    karakter2 = sprites.create(assets.image`
-            Goompion
-        `, SpriteKind.SelectableCharacter)
+    karakter2 = sprites.create(assets.image`Goompion`, SpriteKind.SelectableCharacter)
     karakter2.z = 101
     karakter2.setPosition(73, 67)
     sprites.setDataSprite(karakter2, "player", karakter2)
-    karakter3 = sprites.create(assets.image`
-            myImage
-        `, SpriteKind.SelectableCharacter)
+    karakter3 = sprites.create(assets.image`myImage`, SpriteKind.SelectableCharacter)
     karakter3.z = 101
     karakter3.setPosition(126, 67)
     sprites.setDataSprite(karakter3, "player", karakter3)
     ourCharacters = [karakter1, karakter2, karakter3]
     current_character_index = 1
     character_selector_box = sprites.create(img`
-            ffffffffffffffffffffffffffffffffffff
-                    f2222222222222222222222222222222222f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2................................2f
-                    f2222222222222222222222222222222222f
-                    ffffffffffffffffffffffffffffffffffff
+        ffffffffffffffffffffffffffffffffffff
+        f2222222222222222222222222222222222f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2................................2f
+        f2222222222222222222222222222222222f
+        ffffffffffffffffffffffffffffffffffff
         `, SpriteKind.Background)
     character_selector_box.z = 101
 }
-
-controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
-    
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     if (showCharacterScreen) {
         current_character_index = (current_character_index + 1) % ourCharacters.length
     }
-    
 })
-spriteutils.createRenderable(100, function on_create_renderable(screen2: Image) {
-    
+spriteutils.createRenderable(100, function (screen2) {
     if (showCharacterScreen) {
         screen2.fill(1)
         currently_selected_character = ourCharacters[current_character_index]
         character_selector_box.setPosition(currently_selected_character.x, currently_selected_character.y)
     }
-    
 })
-let character_selector_box : Sprite = null
-let textSprite : TextSprite = null
-let karakter1 : Sprite = null
-let ourCharacters : Sprite[] = []
+let character_selector_box: Sprite = null
+let textSprite: TextSprite = null
+let karakter1: Sprite = null
+let ourCharacters: Sprite[] = []
 let current_character_index = 0
-let currently_selected_character : Sprite = null
-let thePlayer : Sprite = null
+let currently_selected_character: Sprite = null
+let thePlayer: Sprite = null
 let showCharacterScreen = false
-let karakter3 : Sprite = null
-let karakter2 : Sprite = null
-let karakter1_liten : Sprite = null
+let karakter3: Sprite = null
+let karakter2: Sprite = null
+let karakter1_liten: Sprite = null
 let gravity = 0
 game.splash("Welcome To Food Smashers!")
-tiles.setCurrentTilemap(tilemap`
-    level1
-`)
+tiles.setCurrentTilemap(tilemap`level1`)
 gravity = 500
-karakter1_liten = sprites.create(assets.image`
-    karakter1_small
-`, SpriteKind.Player)
-karakter2 = sprites.create(assets.image`
-    Goompion
-`, SpriteKind.Player)
-karakter3 = sprites.create(assets.image`
-    myImage
-`, SpriteKind.Player)
+karakter1_liten = sprites.create(assets.image`karakter1_small`, SpriteKind.Player)
+karakter2 = sprites.create(assets.image`Goompion`, SpriteKind.Player)
+karakter3 = sprites.create(assets.image`myImage`, SpriteKind.Player)
 scene.setBackgroundColor(8)
-scene.setBackgroundImage(assets.image`
-    background
-`)
+scene.setBackgroundImage(assets.image`background`)
 show_character_picker()
-music.play(music.createSong(assets.song`
-        intro sang
-    `), music.PlaybackMode.UntilDone)
+music.play(music.createSong(assets.song`intro sang`), music.PlaybackMode.UntilDone)
